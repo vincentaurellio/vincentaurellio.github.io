d2f36a57506d6641609aedb8d4d9b0d544576ec6
vincent <vincent.aurellio@u.nus.edu>
2026-08-06 16:16:49 +0800

    Add react router for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { RouterProvider } from "react-router";
+import { router } from "./router";
 
 function App() {
-  return <div>Content</div>
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
